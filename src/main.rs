1e56cd1042ed70ce083168245834191703dc7642
金子拓夢 <135446129+IT-F-07@users.noreply.github.com>
2024-09-26 11:51:11 +0900

    eguiのインストール

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use eframe::egui::*;
+
+#[derive(Default)]
+pub struct EguiSample {
+    counter: i32,
+}
+
+impl EguiSample {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for EguiSample {
+    fn save(&mut self, _storage: &mut dyn eframe::Storage) {}
+
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, egui!");
+            if ui.button("Increment").clicked() {
+                self.counter += 1;
+            }
+            ui.label(format!("Counter: {}", self.counter));
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "egui_sample",
+        options,
+        Box::new(|cc| Box::new(EguiSample::new(cc))),
+    );
 }
